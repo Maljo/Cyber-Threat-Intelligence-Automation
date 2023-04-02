bd9d08e3f13660973448fe08d5cb026428b5c8a4
Jawad Malik <4330876+Maljo@users.noreply.github.com>
2023-04-02 13:23:44 +0100

    Create TB_NTA.py

---
+++ TB_NTA.py
@@ -0,0 +1,53 @@
+from pymisp import ExpandedPyMISP, MISPEvent, MISPAttribute
+from scapy.all import *
+import time
+
+# Connect to the MISP instance
+misp = ExpandedPyMISP('https://your.misp.com', 'api-key', ssl=True)
+
+# Define the network interface and filter expression
+iface = 'eth0'
+filter_exp = 'tcp'
+
+# Use Scapy to capture packets
+pkts = sniff(iface=iface, filter=filter_exp, timeout=15)
+
+# Define a list of IP addresses to monitor for suspicious traffic
+monitor_ips = []
+
+# Download Indicators of Compromise (IoCs) from MISP
+misp_events = misp.search(tags=['botnet', 'malware'], type_attribute='ip-src')
+
+for event in misp_events:
+    misp_event = misp.get_event(event['Event']['id'])
+    for attribute in misp_event['Event']['Attribute']:
+        if attribute['type'] == 'ip-src':
+            monitor_ips.append(attribute['value'])
+
+# Define a dictionary to store the packet count for each IP address
+ip_counts = {}
+
+# Loop through the packets and analyze each one
+for pkt in pkts:
+
+    # Check if the packet is an IP packet
+    if IP in pkt:
+
+        # Check if the source or destination IP is in the list of IPs to monitor
+        if pkt[IP].src in monitor_ips or pkt[IP].dst in monitor_ips:
+
+            # Increment the packet count for the IP address
+            if pkt[IP].src in ip_counts:
+                ip_counts[pkt[IP].src] += 1
+            else:
+                ip_counts[pkt[IP].src] = 1
+
+            if pkt[IP].dst in ip_counts:
+                ip_counts[pkt[IP].dst] += 1
+            else:
+                ip_counts[pkt[IP].dst] = 1
+
+# Check if any IP address has sent or received more than 10 packets
+for ip, count in ip_counts.items():
+    if count > 10:
+        print(f'Suspicious traffic from IP address {ip} - {count} packets detected.')
